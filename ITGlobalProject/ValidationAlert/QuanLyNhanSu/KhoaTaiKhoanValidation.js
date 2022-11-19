245522a874746f991c3d4c1d05c3ef440fc1da2a
Dang Van Tuan <tuan.197pm21996@vanlanguni.vn>
2022-11-19 22:37:31 +0700

    update alert style and content validation

--- ITGlobalProject/ValidationAlert/QuanLyNhanSu/KhoaTaiKhoanValidation.js
+++ ITGlobalProject/ValidationAlert/QuanLyNhanSu/KhoaTaiKhoanValidation.js
@@ -3,76 +3,149 @@
         let name = $('#khoataikhoan').text();
         var SweetAlert2Demo = function () {
             var initDemos = function () {
-                swal({
-                    title: name + '?',
-                    text: "Tài khoản sẽ bị khóa và không thể truy cập vào hệ thống!",
-                    type: 'warning',
-                    buttons: {
-                        cancel: {
-                            visible: true,
-                            text: 'Hủy bỏ',
-                            className: 'btn btn-danger'
-                        },
-                        confirm: {
-                            text: 'Đồng ý',
-                            className: 'btn btn-success'
+                if (name.trim() === "KHÓA TÀI KHOẢN") {
+                    swal({
+                        title: 'Khóa Tài Khoản?',
+                        text: "Xác nhận khóa tài khoản?",
+                        type: 'warning',
+                        buttons: {
+                            cancel: {
+                                visible: true,
+                                text: ' Hủy Bỏ ',
+                                className: 'btn btn-danger'
+                            },
+                            confirm: {
+                                text: 'Xác Nhận',
+                                className: 'btn btn-success'
+                            }
                         }
-                    }
-                }).then((khoataikhoans) => {
-                    if (khoataikhoans) {
-                        let ids = $('#idus').val();
-                        let urls = $('#actionKhoaTaiKhoan').data('request-url');
-                        $.ajax({
-                            url: urls,
-                            type: 'POST',
-                            dataType: 'html',
-                            data: { id: ids }
-                        }).done(function (ketqua) {
-                            if (ketqua === "DAKHOA") {
-                                $('#khoataikhoan').replaceWith('<a id="khoataikhoan" style="width:100%" class="btn btn-outline-success btn-sm">MỞ KHÓA TÀI KHOẢN <i class="mdi mdi-lock "></i></a>');
-                                $.when(
-                                    $.getScript("/ValidationAlert/QuanLyNhanSu/KhoaTaiKhoanValidation.js"),
-                                    $.Deferred(function (deferred) {
-                                        $(deferred.resolve);
-                                    })
-                                ).done(function () {
-                                    //place your code here, the scripts are all loaded
-                                });
-                                swal("Đã khóa tài khoản, chọn mở tài khoản để mở lại tài khoản này!", {
-                                    icon: "success",
-                                    buttons: {
-                                        confirm: {
-                                            className: 'btn btn-success'
+                    }).then((khoataikhoans) => {
+                        if (khoataikhoans) {
+                            let ids = $('#idus').val();
+                            let urls = $('#actionKhoaTaiKhoan').data('request-url');
+                            $.ajax({
+                                url: urls,
+                                type: 'POST',
+                                dataType: 'html',
+                                data: { id: ids }
+                            }).done(function (ketqua) {
+                                if (ketqua === "DAKHOA") {
+                                    $('#khoataikhoan').replaceWith('<a id="khoataikhoan" style="width:100%" class="btn btn-outline-success btn-sm">MỞ KHÓA TÀI KHOẢN <i class="mdi mdi-lock "></i></a>');
+                                    $.when(
+                                        $.getScript($('#reloadScriptLock').val()),
+                                        $.Deferred(function (deferred) {
+                                            $(deferred.resolve);
+                                        })
+                                    ).done(function () {
+                                        //place your code here, the scripts are all loaded
+                                    });
+                                    swal("Đã khóa tài khoản!", {
+                                        icon: "success",
+                                        buttons: {
+                                            confirm: {
+                                                className: 'btn btn-success'
+                                            }
                                         }
-                                    }
-                                });
-                            }
-                            else if (ketqua === "DANHSACH")
-                            {
-                                window.location.href($('#actionDanhsach').data('request-url'));
-                            }
-                            else {
-                                $('#khoataikhoan').replaceWith('<a id="khoataikhoan" style="width:100%" class="btn btn-outline-danger btn-sm">KHÓA TÀI KHOẢN <i class="mdi mdi-lock "></i></a>');
-                                $.when(
-                                    $.getScript("/ValidationAlert/QuanLyNhanSu/KhoaTaiKhoanValidation.js"),
-                                    $.Deferred(function (deferred) {
-                                        $(deferred.resolve);
-                                    })
-                                ).done(function () {
-                                    //place your code here, the scripts are all loaded
-                                });
-                                swal("Đã mở khóa tài khoản, tài khoản có thể truy cập vào hệ thống từ bây giờ!", {
-                                    icon: "success",
-                                    buttons: {
-                                        confirm: {
-                                            className: 'btn btn-success'
+                                    });
+                                }
+                                else if (ketqua === "DANHSACH") {
+                                    window.location.href($('#actionDanhsach').data('request-url'));
+                                }
+                                else {
+                                    $('#khoataikhoan').replaceWith('<a id="khoataikhoan" style="width:100%" class="btn btn-outline-danger btn-sm">KHÓA TÀI KHOẢN <i class="mdi mdi-lock "></i></a>');
+                                    $.when(
+                                        $.getScript($('#reloadScriptLock').val()),
+                                        $.Deferred(function (deferred) {
+                                            $(deferred.resolve);
+                                        })
+                                    ).done(function () {
+                                        //place your code here, the scripts are all loaded
+                                    });
+                                    swal("Đã mở khóa tài khoản! Từ bây giờ Tài khoản đã có thể truy cập vào hệ thống!", {
+                                        icon: "success",
+                                        buttons: {
+                                            confirm: {
+                                                className: 'btn btn-success'
+                                            }
                                         }
-                                    }
-                                });
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+                else
+                {
+                    swal({
+                        title: 'Mở Khóa?',
+                        text: "Xác nhận mở khóa tài khoản?",
+                        type: 'warning',
+                        buttons: {
+                            cancel: {
+                                visible: true,
+                                text: ' Hủy Bỏ ',
+                                className: 'btn btn-danger'
+                            },
+                            confirm: {
+                                text: 'Xác Nhận',
+                                className: 'btn btn-success'
                             }
-                        });
-                    }
-                });
+                        }
+                    }).then((khoataikhoans) => {
+                        if (khoataikhoans) {
+                            let ids = $('#idus').val();
+                            let urls = $('#actionKhoaTaiKhoan').data('request-url');
+                            $.ajax({
+                                url: urls,
+                                type: 'POST',
+                                dataType: 'html',
+                                data: { id: ids }
+                            }).done(function (ketqua) {
+                                if (ketqua === "DAKHOA") {
+                                    $('#khoataikhoan').replaceWith('<a id="khoataikhoan" style="width:100%" class="btn btn-outline-success btn-sm">MỞ KHÓA TÀI KHOẢN <i class="mdi mdi-lock "></i></a>');
+                                    $.when(
+                                        $.getScript($('#reloadScriptLock').val()),
+                                        $.Deferred(function (deferred) {
+                                            $(deferred.resolve);
+                                        })
+                                    ).done(function () {
+                                        //place your code here, the scripts are all loaded
+                                    });
+                                    swal("Đã khóa tài khoản!", {
+                                        icon: "success",
+                                        buttons: {
+                                            confirm: {
+                                                className: 'btn btn-success'
+                                            }
+                                        }
+                                    });
+                                }
+                                else if (ketqua === "DANHSACH") {
+                                    window.location.href($('#actionDanhsach').data('request-url'));
+                                }
+                                else {
+                                    $('#khoataikhoan').replaceWith('<a id="khoataikhoan" style="width:100%" class="btn btn-outline-danger btn-sm">KHÓA TÀI KHOẢN <i class="mdi mdi-lock "></i></a>');
+                                    $.when(
+                                        $.getScript($('#reloadScriptLock').val()),
+                                        $.Deferred(function (deferred) {
+                                            $(deferred.resolve);
+                                        })
+                                    ).done(function () {
+                                        //place your code here, the scripts are all loaded
+                                    });
+                                    swal("Đã mở khóa tài khoản!", {
+                                        icon: "success",
+                                        buttons: {
+                                            confirm: {
+                                                className: 'btn btn-success'
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
             };
             return {
                 init: function () {
