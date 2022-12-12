94a49124a02e47cda8d47bedfd00f26e2b1eb800
ĐẶNG VĂN TUẤN <tuan.197pm21996@vanlanguni.vn>
2022-12-12 17:11:34 +0700

    Update function: management position

--- ITGlobalProject/ValidationAlert/Admin/QuanLyVaiTro/SuaVaiTroValidation.js
+++ ITGlobalProject/ValidationAlert/Admin/QuanLyVaiTro/SuaVaiTroValidation.js
@@ -7,34 +7,53 @@
         var ids = $('#ids' + name).val();
         var names = $('#names' + name).val();
         var des = $('#des' + name).val();
+        var depart = $('#depart' + name).val();
 
         $('#id').val(ids);
         $('#name').val(names);
         $('#descript').val(des);
+        $('#bophansua option[value = ' + depart + ']').attr('selected', true);
     });
 
     //Click lưu
-
     $('#luuChinhSua').on('click', function (e) {
+
+        $('#EditRoleValidateResul').text("");
+        $('#MoTaEditRoleValidateResul').text("");
+
         var id = $('#id').val();
         var name = $('#name').val();
         var des = $('#descript').val();
+        var depart = $('#bophansua :selected').val();
 
         var formats = /[`!#$%^&*()+\=\[\]{};':"\\|@_<>\/?~]/;
+        var formatNumber = /[0123456789]/;
+
+        var checkname = true;
+        var checkmota = true;
 
         if (name.length == 0) {
+            checkname = false;
             $('#EditRoleValidateResul').text("Không được bỏ trống thông tin này! Vui lòng nhập đầy đủ.").show();
-        } else if (formats.test(name) == true) {
+        } else if (formats.test(name) == true || formatNumber.test(name) == true) {
+            checkname = false;
             $('#EditRoleValidateResul').text("Tên chức danh không hợp lệ! Vui lòng kiểm tra lại.").show();
         } else if (name.length > 50) {
+            checkname = false;
             $('#EditRoleValidateResul').text("Tên chức danh chỉ tối đa 50 ký tự! Vui lòng kiểm tra lại.").show();
         }
-        else {        
+
+        if (des.length > 200) {
+            checkmota = false;
+            $('#MoTaEditRoleValidateResul').text("Mô tả chức danh chỉ tối đa 200 ký tự! Vui lòng kiểm tra lại.").show();
+        }
+
+        if (checkname == true && checkmota == true) {        
             var formData = new FormData();
             formData.append('id', id);
             formData.append('name', name);
             formData.append('description', des);
-
+            formData.append('depart', depart);
             let urls = $('#actionChinhSua').data('request-url');
             $.ajax({
                 url: urls,
